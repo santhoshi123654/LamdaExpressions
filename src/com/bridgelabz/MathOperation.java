57634f97db48b1a2b5db58b9935f2a0175fc703f
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-12-06 16:32:55 +0530

    Added:Add,Sub and Division Calculation.

---
+++ src/com/bridgelabz/MathOperation.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class MathOperation {
+	
+	public int add(int a,int b) {
+	
+		return a+b;		
+	}
+	public int sub(int a,int b) {
+		
+		return a-b;		
+	}
+	public int div(int a,int b) {
+		
+		return a/b;	
+	}
+}
